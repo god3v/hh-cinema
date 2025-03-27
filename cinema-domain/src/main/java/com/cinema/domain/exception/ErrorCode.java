3d48d2c2aab3d1d8a76941ae57f4dd24f36ca956
god3v <sooon9254@naver.com>
2025-03-28 01:10:32 +0900

    refactor: 도메인 모델 수정

---
+++ cinema-domain/src/main/java/com/cinema/domain/exception/ErrorCode.java
@@ -0,0 +1,8 @@
+package com.cinema.domain.exception;
+
+public enum ErrorCode {
+    BUSINESS_ERROR,
+    NOT_FOUND,
+    DB_ERROR,
+    CLIENT_ERROR,
+}
--- cinema-domain/src/main/java/com/cinema/domain/model/ScreenSeat.java
+++ cinema-domain/src/main/java/com/cinema/domain/model/ScreenSeat.java
@@ -1,8 +1,11 @@
 package com.cinema.domain.model;
 
+import lombok.Builder;
+
+@Builder
 public record ScreenSeat(
         Long id, // 상영관 좌석 ID
-        Screen screen ,// 상영관
+        Long screenId, // 상영관
         char row, // 좌석 위치 행
         int col // 좌석 위치 열
 ) {
