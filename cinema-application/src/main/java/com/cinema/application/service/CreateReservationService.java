b572fff94b6ae4b757d5fb09394b6174080e5e48
god3v <sooon9254@naver.com>
2025-03-31 03:53:00 +0900

    feat: 예약 API AOP 분산락 -> 함수형 분산락 변경, MessageService 추가

--- cinema-application/src/main/java/com/cinema/application/service/CreateReservationService.java
+++ cinema-application/src/main/java/com/cinema/application/service/CreateReservationService.java
@@ -2,13 +2,16 @@ package com.cinema.application.service;
 
 import com.cinema.application.dto.CreateReservationCommand;
 import com.cinema.application.dto.CreateReservationResult;
+import com.cinema.application.event.MessageService;
 import com.cinema.application.mapper.ReservationMapper;
 import com.cinema.application.port.in.CreateReservationUseCase;
 import com.cinema.application.port.out.TicketReservationPort;
+import com.cinema.common.aop.DistributedLock;
+import com.cinema.common.aop.DistributedLockUtil;
 import com.cinema.domain.model.TicketReservation;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -18,18 +21,24 @@ public class CreateReservationService implements CreateReservationUseCase {
 
     private final ReservationValidator reservationValidator;
     private final TicketReservationPort reservationPort;
+    private final DistributedLockUtil lockUtil;
+    private final MessageService messageService;
 
     @Override
-//    @Transactional
-    @DistributedLock(key = "#command.scheduleId")
+    @Transactional
+//    @DistributedLock(key = "#command.scheduleId")
     public List<CreateReservationResult> createReservation(CreateReservationCommand command) {
         TicketReservation reservation = ReservationMapper.toDomain(command);
 
-        // 예약 유효성 검증
-        reservationValidator.validate(reservation);
+        String lockKey = "lock:" + command.scheduleId();
+        return lockUtil.executeWithLock(lockKey, 5, 3, () -> {
+            reservationValidator.validate(reservation);
 
-        List<Long> reservedIds = reservationPort.saveReservations(reservation);
+            List<Long> reservedIds = reservationPort.saveReservations(reservation);
 
-        return reservationPort.findReservations(reservedIds);
+            List<CreateReservationResult> reservations = reservationPort.findReservations(reservedIds);
+            messageService.send(reservations);
+            return reservations;
+        });
     }
 }
